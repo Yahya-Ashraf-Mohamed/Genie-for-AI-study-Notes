cd022366ce383dfb73298c910c4e4ec462a3cff6
Moustafa Ashamwy <mouashmawy@gmail.com>
2024-12-17 04:16:41 +0200

    started working on quizzes

--- BackEnd-app/main.py
+++ BackEnd-app/main.py
@@ -77,48 +77,6 @@ async def search_study_material(title: str):
 
 
 
-###############     Quiz        ##############
-
-@router.get("/quizzes/", tags=["Quizzes"])
-async def read_all_quizzes():
-    data = quiz_collection.find()
-    return get_all_quizzes(data)
-
-
-@app.get("/quiz/{quiz_id}", tags=["Quizzes"])
-async def read_quiz(quiz_id: str):
-    try:
-        quiz = quiz_collection.find_one({"_id": ObjectId(quiz_id)})
-        if quiz:
-            return mongo_to_dict(quiz)
-        else:
-            raise HTTPException(status_code=404, detail="quiz not found")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@router.post("/quiz/", tags=["Quizzes"])
-async def create_quiz(quiz:Quiz):
-    try:
-        res = quiz_collection.insert_one(dict(quiz))
-        return {"status code": 200, "id": str(res.inserted_id)}
-    except Exception as e:
-        return HTTPException(status_code=500, detail=f"{e}")
-
-@app.get("/search_quizzes/", tags=["Quizzes"]) ##to be edited
-async def search_study_material(title: str):
-    try:
-       
-        results = study_material_collection.find({"title": {"$regex": title, "$options": "i"}})
-        materials = [StudyMaterial(**result) for result in results]
-        if not materials:
-            raise HTTPException(status_code=404, detail="No study materials found with that title.")
-        
-        return {"status_code": 200, "found_materials": materials}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error searching for quizzes: {e}")
-
 
 
 ###############     Study material       ##############
@@ -170,7 +128,7 @@ async def search_study_material(title: str):
 
 
 
-###############     Create Chat       ##############
+###############      Chat conversation: Q&A       ##############
 
 @app.get("/chat/{chat_id}", tags=["chats"])
 async def read_chat(chat_id: str):
@@ -223,16 +181,63 @@ async def send_message(message: IncomingChatMessage):
         )
 
 
-
         return {"status code": 200, "message": "Message added successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{e}")
 
-
-
 def AI_response(message):
     return f"This is the AI answer to::: {message}"
 
+
+
+
+###############     Quiz        ##############
+
+@router.post("/createquiz/", tags=["Quizzes"])
+async def create_quiz(quiz:Quiz):
+    try:
+        res = quiz_collection.insert_one(dict(quiz))
+        return {"status code": 200, "id": str(res.inserted_id)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{e}")
+
+
+@router.post("/validatequizanswers/", tags=["Quizzes"])
+async def validate_quiz_answers(quiz:Quiz):
+    pass
+
+
+# @router.get("/quizzes/", tags=["Quizzes"])
+# async def read_all_quizzes():
+#     data = quiz_collection.find()
+#     return get_all_quizzes(data)
+
+
+# @app.get("/quiz/{quiz_id}", tags=["Quizzes"])
+# async def read_quiz(quiz_id: str):
+#     try:
+#         quiz = quiz_collection.find_one({"_id": ObjectId(quiz_id)})
+#         if quiz:
+#             return mongo_to_dict(quiz)
+#         else:
+#             raise HTTPException(status_code=404, detail="quiz not found")
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+
+
+# @app.get("/search_quizzes/", tags=["Quizzes"]) ##to be edited
+# async def search_study_material(title: str):
+#     try:
+       
+#         results = study_material_collection.find({"title": {"$regex": title, "$options": "i"}})
+#         materials = [StudyMaterial(**result) for result in results]
+#         if not materials:
+#             raise HTTPException(status_code=404, detail="No study materials found with that title.")
+        
+#         return {"status_code": 200, "found_materials": materials}
+    
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Error searching for quizzes: {e}")
 
 
 

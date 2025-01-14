adc337bf4e922828cc1b8429d19c0aec355ad72e
Fatma Taha <94760329+fatmaT2001@users.noreply.github.com>
2025-01-14 18:47:55 +0200

    Update main.py
    allowing all rouets

--- BackEnd-app/main.py
+++ BackEnd-app/main.py
@@ -19,10 +19,9 @@ os.environ["GROQ_API_KEY"] = "gsk_MD15s2PSEkE8H1e3oJSeWGdyb3FYACFy9BduZJb5zybnKR
 
 app = FastAPI(title="Genie - AI Study Assistant")
 
-origins = ['http://localhost:3000']  #
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_headers=["*"],
     allow_methods=["*"],
